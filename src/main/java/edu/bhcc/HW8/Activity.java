8448bc26a1e0d91e2921d61e930c798e83a33ed9
Quinlin Taylor <102311011+qnobletaylor@users.noreply.github.com>
2024-12-10 13:18:39 -0500

    added record class Activity for activities that will be saved in database

---
+++ src/main/java/edu/bhcc/HW8/Activity.java
@@ -0,0 +1,82 @@
+package edu.bhcc.HW8;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ * Record class for use by JPA database.
+ * Has attributes for id (primary key), route (name of place or route for the activity), miles (distance covered in
+ * activity), and date (date the activity was done).
+ */
+public class Activity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String route;
+  private Double miles;
+  private String date;
+
+  /**
+   * Default constructor
+   */
+  protected Activity() {
+  }
+
+  /**
+   * Parameterized constructor for creating new records.
+   */
+  public Activity(String route, Double miles, String date) {
+    this.route = route;
+    this.miles = miles;
+    this.date = date;
+  }
+
+  // Getters and setters
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getRoute() {
+    return route;
+  }
+
+  public void setRoute(String route) {
+    this.route = route;
+  }
+
+  public Double getMiles() {
+    return miles;
+  }
+
+  public void setMiles(Double miles) {
+    this.miles = miles;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+
+  /**
+   * toString method to print a neatly formatted summary of an activity.
+   *
+   * @return the formatted string of attributes
+   */
+  @Override
+  public String toString() {
+    return String.format(
+        "Activity [\n\tid:%d,\n\troute:'%s',\n\tmiles:%f,\n\tdate:'%s'\n]",
+        id, route, miles, date
+    );
+  }
+}
--- src/main/java/edu/bhcc/HW8/ActivityLogApplication.java
+++ src/main/java/edu/bhcc/HW8/ActivityLogApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Hw8Application {
+public class ActivityLogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Hw8Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ActivityLogApplication.class, args);
+  }
 
 }
